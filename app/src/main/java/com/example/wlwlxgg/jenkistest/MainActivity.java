c7c27970b6e9f798454c4da1212db9d7b0b422d1
Lei Wang(CIT WSD-BJ) <wanglei18@asiainfo.com>
2018-04-10 16:24:31 +0800

    测试参数化构建

--- app/src/main/java/com/example/wlwlxgg/jenkistest/MainActivity.java
+++ app/src/main/java/com/example/wlwlxgg/jenkistest/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.wlwlxgg.jenkistest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.tv);
+        String apiEvironment = BuildConfig.ApiEnvironment;
+        if (apiEvironment.equals("local")) textView.setText("本地");
+        else if (apiEvironment.equals("pro")) textView.setText("生产环境");
+        else if (apiEvironment.equals("dev")) textView.setText("开发环境");
     }
 }
